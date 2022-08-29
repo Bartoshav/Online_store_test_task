bcf6d047e5ca26f6477e29a16328584ccebf0842
Bartoshav <101507600+Bartoshav@users.noreply.github.com>
2022-08-29 10:14:54 +0300

    Pagination
    Pagination was added

--- online_store/store_api/views.py
+++ online_store/store_api/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 from .models import Product, Order
 from .serializers import ProductSerializer, OrderSerializer
+
+class APIListPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 10000
 
 # Create your views here.
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    pagination_class = APIListPagination
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    pagination_class = APIListPagination
